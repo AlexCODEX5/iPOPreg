017f91341d11ff14e7167f663cda0dbf84490f90
AlexCODEX5 <32914572+AlexCODEX5@users.noreply.github.com>
2019-10-31 21:31:50 -0500

    Se creo el Login de forma casi catastrofica por agregar Windows.Forms

---
+++ iPOPreg/BDConexion.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using System.Data;
+using System.Windows;
+
+namespace iPOPreg
+{
+    public class BDConexion
+    {
+        //Cada data reader se genera por cada metodo de operacion de base de datos
+        //Cada table se define por la funcion
+        private string connectionString = "";
+        string query;
+        string table;
+
+        public string CadenaTable()
+        {
+            return table;
+        }
+
+        public void SetCadenaTable(string newTable)
+        {
+            table = newTable;
+        }
+
+        public string CadenaConexion()
+        {
+            return connectionString;
+        }
+
+        public void SetCadenaConexion(string datasource, string port, string username, string password, string database)
+        {
+            connectionString = $"datasource={datasource};port={port};username={username};password={password};database={database}";
+        }
+
+        public void GetConect(MySqlConnection bdConnect)
+        {
+            try
+            {
+                bdConnect.Open();
+                MessageBox.Show("Conexion exitosa");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void DisConect(MySqlConnection bdConnect)
+        {
+            try
+            {
+                bdConnect.Close();
+                MessageBox.Show("Conexion Cerrada Correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "La conexion no se cerró Correctamente");
+            }
+        }
+
+        public MySqlDataReader VerifyUser(MySqlConnection bdConnect, string table, string Usuario, string Contraseña)
+        {
+            query = $"SELECT * FROM `{table}` WHERE Usuario = '{Usuario}' AND CONTRASEÑA = '{Contraseña}'";
+            MySqlCommand commandDatabase = new MySqlCommand(query, bdConnect);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            reader = commandDatabase.ExecuteReader();
+            /*string[] row = { reader.GetString(0),  };
+            return row ;*/
+            return reader;
+        }
+
+        public MySqlDataReader UserList(MySqlConnection bdConnect, string table)
+        {
+            query = $"SELECT `Nombre`,`Apellidos`,`ESTADO` FROM `{table}` WHERE ESTADO = 'A'";
+            MySqlCommand commandDatabase = new MySqlCommand(query, bdConnect);
+            commandDatabase.CommandTimeout = 60;
+            MySqlDataReader reader;
+            reader = commandDatabase.ExecuteReader();
+            return reader;
+        }
+    }
+}
--- iPOPreg/MainWindow.xaml.cs
+++ iPOPreg/MainWindow.xaml.cs
@@ -23,9 +23,122 @@ namespace iPOPreg
     /// </summary>
     public partial class MainWindow : Window
     {
+        BDConexion verifyUser = new BDConexion();
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Salir_Login_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Minimizar_Login_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+
+        //Se copia la base de datos de docentes y se le puede cambiar por responsables
+        //Cada cadena se obtiene por el data set por defecto que genera el Login
+        private void Verificar_Login_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection verifyUserCon = new MySqlConnection(verifyUser.CadenaConexion());//Conexion
+
+            try
+            {
+                verifyUserCon.Open();
+                MessageBox.Show("La base de datos se conecto correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
+            if (verifyUserCon.State == ConnectionState.Open)
+            {
+                MySqlDataReader reader;
+                verifyUser.SetCadenaTable("docentes");
+                reader = verifyUser.VerifyUser(verifyUserCon, verifyUser.CadenaTable(), User_Login.Text, Psw_Login.Password);
+                if (reader.HasRows)
+                {
+                    MessageBox.Show("Login Correcto", "Exito", MessageBoxButton.OK, MessageBoxImage.Information);
+                    Verificar_Login.Content = "Acceso";
+                    Panel_control panelControl = new Panel_control();
+                    panelControl.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Usuario o contraseña incorrectos, verifique si esta registrado");
+                }
+            }
+
+            verifyUserCon.Close();
+            MySqlConnection.ClearPool(verifyUserCon);
+        }
+
+        private void Panel_Login_Loaded(object sender, RoutedEventArgs e)
+        {
+            User_Login.Focus();
+
+            MySqlConnection UserList = new MySqlConnection(verifyUser.CadenaConexion());
+            try
+            {
+                UserList.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            if (UserList.State == ConnectionState.Open)
+            {
+                MySqlDataReader reader;
+                reader = verifyUser.UserList(UserList, verifyUser.CadenaTable());
+                //Aqui existe un metodo para ser agregado a clase
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        string[] row = { reader.GetString(0), reader.GetString(1) };
+                        UserList_Login.Items.Add($"{row[0]} {row[1]}");
+                    }
+                }
+            }
+            UserList.Close();
+            MySqlConnection.ClearPool(UserList);
+        }
+
+        private void RefreshList_Login_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConnection UserList = new MySqlConnection(verifyUser.CadenaConexion());
+            try
+            {
+                UserList.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            if (UserList.State == ConnectionState.Open)
+            {
+                MySqlDataReader reader;
+                reader = verifyUser.UserList(UserList, verifyUser.CadenaTable());
+                if (reader.HasRows)
+                {
+                    UserList_Login.Items.Clear();
+                    while (reader.Read())
+                    {
+                        string[] row = { reader.GetString(0), reader.GetString(1) };
+                        UserList_Login.Items.Add($"{row[0]} {row[1]}");
+                    }
+                }
+            }
+            UserList.Close();
+            MySqlConnection.ClearPool(UserList);
         }
     }
 }
